89ef161ff400c64e4f584aaf4b52ca69a1914891
Jet Basrawi <jet_basrawi@hotmail.com>
2016-06-05 17:56:35 +0100

    Added comments further comments to dispatcher

--- dispatcher.go
+++ dispatcher.go
@@ -1,10 +1,29 @@
+// Copyright 2016 Jet Basrawi. All rights reserved.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+  
+
 package ycq
 
 import (
 	"fmt"
 )
 
-//Dispatcher is the interface that should be implemented by command dispatchers
+//Dispatcher is the interface that should be implemented by command dispatcher
+//
+//The dispatcher is the mechanism through which commands are distributed to
+//the appropriate command handler.
+//
+//Command handlers are registered with the dispatcher for a given command type. 
+//It is good practice in CQRS to have only one command handler for a given command. 
+//When a command is passed to the dispatcher it will look for the registered command
+//handler and call that handler's Handle method passing the command message as an
+//argument. 
+//
+//Commands contained in a CommandMessage envelope are passed to the Dispatcher via 
+//the dispatch method.
 type Dispatcher interface {
 	Dispatch(CommandMessage) error
 	RegisterHandler(CommandHandler, ...interface{}) error
@@ -23,7 +42,7 @@ func NewInMemoryDispatcher() *InMemoryDispatcher {
 	return b
 }
 
-//Dispatch passes the EventMessage on to all registered command handlers.
+//Dispatch passes the CommandMessage on to all registered command handlers.
 func (b *InMemoryDispatcher) Dispatch(command CommandMessage) error {
 	if handler, ok := b.handlers[command.CommandType()]; ok {
 		return handler.Handle(command)
